729bcf5cb900677a48b72ac12218dc2f190a5e2d
Gabriele De Rosa <derosagabriele@outlook.it>
2021-03-30 17:17:45 +0200

    Change pkg name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="jarvis",
+    name="jarvis-core",
     version="1.0.0-beta",
     author="derogab",
     author_email="derosagabriele@outlook.it",
